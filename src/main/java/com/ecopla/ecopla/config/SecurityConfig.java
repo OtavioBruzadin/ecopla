b5995c1427e75d4b690c80f7bf09defd22574769
Otávio Bruzadin <31077442+OtavioBruzadin@users.noreply.github.com>
2025-05-09 08:24:40 -0300

    Adding endpoints

---
+++ src/main/java/com/ecopla/ecopla/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.ecopla.ecopla.config;
+
+import com.ecopla.ecopla.security.AuthTokenFilter;
+import com.ecopla.ecopla.service.UsuarioService;
+import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+    private final UsuarioService usuarioService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(UsuarioService usuarioService, PasswordEncoder passwordEncoder) {
+        this.usuarioService = usuarioService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(usuarioService)
+                .passwordEncoder(passwordEncoder)
+                .and().build();
+    }
+
+    @Bean
+    public AuthTokenFilter authTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/auth/signup-admin").hasRole("ADMIN")
+                .requestMatchers("/api/auth/signup").permitAll()
+                .requestMatchers("/api/usuarios/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
